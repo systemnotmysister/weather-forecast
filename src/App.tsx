f8b555576bf910abe4ee60a5191f3def6603e33f
Chronic Insomnia <artdbss@gmail.com>
2020-04-16 21:10:41 +0300

    bla

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { cityListItem } from "./cityListItem";
+import { cityListItem } from "./component/cityListItem";
 import Foo from './component/Foo'
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AppRoute from './component/AppRouter'
+import AppRouter from './component/AppRouter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-<Foo cityListItem={cityListItem}/>
-<AppRoute cityListItem={cityListItem}/>
+       <header className="App-header"></header>
+         <Foo cityListItem={cityListItem}/>
+         
     </div>
   );
 }
-
-
 export default App;
