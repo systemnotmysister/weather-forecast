696f53dcdd5608b9920b12bbd8c06c7b6c332aa1
Chronic Insomnia <artdbss@gmail.com>
2020-04-16 21:15:37 +0300

    deleted excess props

--- src/component/Foo.tsx
+++ src/component/Foo.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Row, Col} from 'antd';
 import 'antd/dist/antd.css';
+
 
 function Foo(props:any) {
   console.log(props.cityListItem);
   return (
     <div>
     {props.cityListItem.map((it:any) => {
-     return( <Row>
+     return ( <Row>
         <Col span={2}> {it.name} </Col>
          <Col span={2}> <img height = "20" src =  
          {it.sunshine === 50 ? "https://image.flaticon.com/icons/svg/861/861059.svg":
            it.sunshine > 50 ? "https://image.flaticon.com/icons/svg/979/979585.svg" :
-             it.sunshine < 50 ? "https://image.flaticon.com/icons/svg/148/148828.svg": "https://image.flaticon.com/icons/svg/178/178338.svg" }/>
+             it.sunshine < 50 ? "https://image.flaticon.com/icons/svg/148/148828.svg": "https://image.flaticon.com/icons/svg/178/178338.svg" }
+             ></img>
              </Col> 
              <Col span={2}>{it.temp} </Col>
       </Row> );
