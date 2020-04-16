0bcbcaab379dbbccaacf7347f57e1fac62b910d7
Chronic Insomnia <artdbss@gmail.com>
2020-04-16 20:58:11 +0300

    this had to assign a path to id

---
+++ src/component/AppRouter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import  {cityListItem}  from "./cityListItem";
+import { useParams } from "react-router-dom";
+import Fu from './UseParam';
+
+const AppRouter = () => {
+    return (
+       <Router>  
+         <Route path ="/id:" child = {<Fu/>}/>
+       </Router>)  
+ }
+ export default AppRouter;
+ 
