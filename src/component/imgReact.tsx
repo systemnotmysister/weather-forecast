2d415626f1fa0cae32bba5165543b9b1ad9baa7d
Chronic Insomnia <artdbss@gmail.com>
2020-04-18 01:05:36 +0300

    image file

---
+++ src/component/imgReact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function imgReact () {
+  return (
+    <div>
+     <img src=" https://storage.googleapis.com/zingchart-blog/zing-content/2016/06/react-1.png"/>
+    </div>
+  );
+}
+export default imgReact;
