cc109cf4b0b9ab842f22b0e0c638acf52a496a0e
Chronic Insomnia <artdbss@gmail.com>
2020-04-18 01:04:39 +0300

    added ability to render React img

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { cityListItem } from "./component/cityListItem";
 import Foo from './component/Foo'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AppRouter from './component/AppRouter'
+import { BrowserRouter as Router, Route, Link , Switch} from "react-router-dom";
+import imgReact from "./component/imgReact";
+
 
 function App() {
   return (
     <div className="App">
        <header className="App-header"></header>
          <Foo cityListItem={cityListItem}/>
-         
+    <div>
+       <Router>  
+           <Link to="/react-route">я учу реакт роут</Link>
+             <Switch>         
+                <Route path="/react-route"> 
+                {imgReact}
+                </Route>
+             </Switch>
+       </Router>
+   </div>  
     </div>
   );
 }
