b946a6b72687bb3b24923a4783808e8bbca5d2f9
Chronic Insomnia <artdbss@gmail.com>
2020-05-13 16:24:46 +0300

    bla

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link , Switch, Redirect} from "react-router-dom";
 import AppRouter from "./component/AppRouter";
-import CityItem from "./component/CityItem"; 
 import CityId from './component/CityId';
 import componentDidMount from './component/Async';
 import GetData from "./component/Async";
 
 
+
+
+
   function App() {
+   
     return (
       <div className="App">
      
-        { CityId.map ((it) =>  <GetData   id = {it.id} /> ) } 
+        { CityId.map ((it) =>  <GetData  props = {it.id}/> ) } 
      
 
        <AppRouter/>
--- src/component/Async.tsx
+++ src/component/Async.tsx
@@ -4,21 +4,21 @@ import { render } from 'react-dom';
 export const apiKey = 'cb4a1782dbd75c2ffdde3b033b2752ee'
 
 
-    class GetData extends Component {
-     constructor(props: Readonly<{}>) {
-         super(props);
-       this.state = {
-     loading: false
-  }
-
+  class GetData extends Component<{id:number}> { 
+  constructor(props: any) {
+      super(props);
+    this.state = {
+  loading: false
 }
+}
+
  async componentDidMount() {
    console.log('fetching started...')
   this.setState({loading: true})
         try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${this.props.id}&appid=${apiKey}`)
         const data = await response.json()
-        this.state = ({ data: data });
+        this.state = {data}
     } catch (e) {
         console.error(e) 
     }
@@ -38,7 +38,6 @@ export const apiKey = 'cb4a1782dbd75c2ffdde3b033b2752ee'
                 {this.state.data.visibility}
                 {this.state.data.temp}
              </li>
-            }
           </ul>
        </div>
       )}
