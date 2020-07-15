21221d18847545ff7f7ed71657a9c935bec1b2b3
Chronic Insomnia <artdbss@gmail.com>
2020-07-15 18:44:59 +0300

    all almost done

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Switch } from "react-router-dom";
 import SecondRequest from "./component/SecondRequest";
 import AppRouter from "./component/AppRouter";
 import { Table } from "antd";
-import { columns } from "./component/Table";
+
 
 
 function App() {
--- src/component/SecondRequest.tsx
+++ src/component/SecondRequest.tsx
@@ -1,22 +1,16 @@
-
+import { Table, Tag  } from 'antd';
 import { render } from 'react-dom';
-import { DatePicker, message } from 'antd';
 import React, { Component, useState, useEffect } from "react";
 import ReactDOM from "react-dom"
-import { Row, Col, Divider, Table } from "antd";
 import { apiKey } from "./FirstRequest";
 import { useParams, Route } from "react-router-dom";
-import { stringify } from "querystring";
-import { listenerCount } from "process";
-import { columns as columns } from './Table';
-import imgReact from './imgReact';
+
+const { Column, ColumnGroup } = Table;
 
 function GetData2() {
 
 	let { id } = useParams();
-	let dataSource;
 
-	const [loading, setLoading] = useState(false);
 
 	async function getData(id: any) {
 		const response = await fetch(
@@ -31,33 +25,35 @@ function GetData2() {
 
 	useEffect(() => {
 		getData(id).then((dataResponse) => {
-console.log(data);
+ console.log(data);
 
 			if (dataResponse) {
 		
-			const bla =	dataResponse.list.map((element: any) => {
+			const dataSource =	dataResponse.list.map((element: any) => {
 				
 					return (
 							
-						{dt_txt: element.dt_txt,
-						feels_like: Math.round(element.main.feels_like - 273)  ,
-						humidity: element.main.humidity,
-						pressure: element.main.pressure,
-						temp: Math.round(element.main.temp - 273),
-						temp_min: Math.round(element.main.temp_min - 273 ) ,
-						temp_max: Math.round(element.main.temp_max - 273)	,
-						speed: element.wind.speed,
-						description:	element.weather[0].description,
-						weatherIconID: element.weather[0].id,
-						weatherIcon: element.weather[0].icon}
+						{
+							dt_txt: element.dt_txt,
+							feels_like: Math.round(element.main.feels_like - 273)  ,
+							humidity: element.main.humidity,
+							pressure: element.main.pressure,
+							temp: Math.round(element.main.temp - 273),
+							temp_min: Math.round(element.main.temp_min - 273 ) ,
+							temp_max: Math.round(element.main.temp_max - 273)	,
+							speed: element.wind.speed,
+							description: element.weather[0].description,
+							weatherIconID: element.weather[0].id,
+							weatherIcon: element.weather[0].icon
+						}
                   
 					)
 
 				})
 				
 				console.log('got it ');
-				console.log(bla);
-				setData(bla)
+				console.log( dataSource);
+				setData(dataSource)
 			
 			}
 		})
@@ -67,9 +63,33 @@ console.log(data);
 
 
 	if (data) {
-		return (              
-			<Table dataSource={data} columns={columns} />
-		)
+		return (
+			<Table dataSource={data}>
+
+				<Column title="date  time" dataIndex="dt_txt" key="dt_txt" />
+				<Column title="feels_like" dataIndex="feels_like" key="feels_like" />
+				<Column title="temp_min" dataIndex="temp_min" key="temp_min" />
+				<Column title="temp_max" dataIndex="temp_max" key="temp_max" />
+				<Column title="temp" dataIndex="temp" key="temp" />
+
+
+				<Column title="humidity" dataIndex="humidity" key="humidity" />
+				<Column title="pressure" dataIndex="pressure" key="pressure" />
+				<Column title="wind_speed" dataIndex="speed" key="speed" />
+				<Column title="description" dataIndex="description" key="description" />
+
+				<Column
+					title="weatherIcon"
+					dataIndex="weatherIcon"
+					key="weatherIcon"
+					render={(weatherIcon) => <div key={weatherIcon}>
+						<img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} />
+					</div>}
+						
+				/>
+			
+			</Table>)
+
 	}
 
 	else { 
